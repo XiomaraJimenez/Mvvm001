0060009f7728b4ebb8d94bb8a68d63c596bccd9a
xiomararosado.0491@hotmail.com <xiomararosado.0491@hotmail.com>
2020-10-22 19:03:29 +0100

    This is my second commit - adding various view and view models

---
+++ app/src/main/java/com/example/mvvm001/data/QuoteRepository.kt
@@ -0,0 +1,23 @@
+package com.example.mvvm001.data
+
+class QuoteRepository private constructor(private val quoteDao: FakeQuoteDao) {
+
+    fun addQuote(quote: Quote) {
+// ad the quote to the repository
+        quoteDao.addQuote(quote)
+    }
+
+    fun getQuotes() = quoteDao.getQuotes()
+
+    companion object {
+        @Volatile
+        private var instance: QuoteRepository? = null
+
+        fun getInstance(quoteDao: FakeQuoteDao) =
+            instance ?: synchronized(this) {
+                instance ?: QuoteRepository(quoteDao).also { instance = it }
+
+
+            }
+    }
+}
